53c4114d447cb05aafa8140d55162e03809d8b5e
yatsuna827 <yatsuna_827@yahoo.co.jp>
2020-01-13 04:10:56 +0900

    強制色違い/強制夢特性/性別固定の処理に対応

--- SwShRNGLibrary/Generator.cs
+++ SwShRNGLibrary/Generator.cs
@@ -12,20 +12,21 @@ namespace SwShRNGLibrary
         public ulong seed { get; private set; }
         private const ulong FIXEDSEED = 0x82A2B175229D6A5B;
         private Nature[] HighNature = new Nature[] { Nature.Adamant, Nature.Naughty, Nature.Brave, Nature.Impish, Nature.Lax, Nature.Rash, Nature.Sassy, Nature.Hasty, Nature.Jolly, Nature.Naive, Nature.Hardy, Nature.Docile, Nature.Quirky };
-        public Pokemon.Individual Generate(int FlawlessIVs, Pokemon.Species poke, bool allowHiddenAbility, Gender FixGender = Gender.Genderless)
+        public Pokemon.Individual Generate(RaidBattleSlot slot)
         {
             Xoroshiro128p rng = new Xoroshiro128p(seed, FIXEDSEED);
+            var poke = slot.pokemon;
 
             uint EC = (uint)(rng.GetRand() & 0xFFFFFFFF);
             uint DummyID = (uint)(rng.GetRand() & 0xFFFFFFFF);
             uint PID = (uint)(rng.GetRand() & 0xFFFFFFFF);
             uint ShinyValue = (((DummyID ^ PID) >> 16) ^ ((DummyID ^ PID) & 0xFFFF));
             bool isShily = ShinyValue < 16;
-            bool isSquare = ShinyValue == 0;
+            bool isSquare = (slot.ForceShiny && !isShily) || ShinyValue == 0;
 
             uint[] IVs = new uint[6];
             int i = 0;
-            while (i < FlawlessIVs)
+            while (i < slot.FlawlessIVs)
             {
                 uint stat = (uint)rng.GetRand(6);
                 if (IVs[stat] == 31) continue;
@@ -35,11 +36,11 @@ namespace SwShRNGLibrary
             for (int k = 0; k < 6; k++)
                 if (IVs[k] == 0) IVs[k] = (uint)(rng.GetRand() & 0x1f);
 
-            uint Ability = allowHiddenAbility ? (uint)rng.GetRand(3) : (uint)rng.GetRand(2);
-            Gender gender = poke.GenderRatio.isFixed() ? Gender.Genderless : ((uint)rng.GetRand(253) < (uint)poke.GenderRatio ? Gender.Female : Gender.Male);
+            uint Ability = slot.ForceHiddenAbility ? 2 : (slot.allowHiddenAbility ? (uint)rng.GetRand(3) : (uint)rng.GetRand(2));
+            Gender gender = slot.FixedGender != Gender.Genderless || poke.GenderRatio == GenderRatio.Genderless ? slot.FixedGender : ((uint)rng.GetRand(253) < (uint)poke.GenderRatio ? Gender.Female : Gender.Male);
             Nature nature = poke.FormName == "ハイ" ? HighNature[rng.GetRand(13)] : (Nature)rng.GetRand(25);
 
-            return poke.GetIndividual(60, IVs, EC, PID, nature, Ability, gender).SetShinyType(isShily ? (isSquare ? ShinyType.Square : ShinyType.Star) : ShinyType.NotShiny);
+            return slot.pokemon.GetIndividual(60, IVs, EC, PID, nature, Ability, gender).SetShinyType(isShily ? (isSquare ? ShinyType.Square : ShinyType.Star) : ShinyType.NotShiny);
         }
         public void Advance(uint num = 1) { seed += FIXEDSEED * num; }
         public void Back(uint num = 1) { seed -= FIXEDSEED * num; }
---
+++ SwShRNGLibrary/RaidBattleSlot.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwShRNGLibrary
+{
+    public class RaidBattleSlot
+    {
+        public readonly Pokemon.Species pokemon;
+        public readonly bool allowHiddenAbility;
+        public readonly int FlawlessIVs;
+        public bool ForceHiddenAbility { get; private set; }
+        public Gender FixedGender { get; private set; }
+        public Nature FixedNature { get; private set; } = Nature.other;
+        public bool ForceShiny { get; private set; }
+        public string Label { get; private protected set; }
+        internal RaidBattleSlot BeForceHiddenAbility() { ForceHiddenAbility = true; return this; }
+        internal RaidBattleSlot BeForceShiny() { ForceShiny = true; return this; }
+        internal RaidBattleSlot FixGender(Gender gender) { FixedGender = gender; return this; }
+        internal RaidBattleSlot FixNature(Nature nature) { FixedNature = nature; return this; }
+
+
+        internal RaidBattleSlot(string name, bool allowHA, int fix)
+        {
+            pokemon = Pokemon.GetPokemon(name);
+            allowHiddenAbility = allowHA;
+            FlawlessIVs = fix;
+
+            switch (pokemon.GenderRatio)
+            {
+                case GenderRatio.MaleOnly:
+                    FixedGender = Gender.Male; break;
+                case GenderRatio.FemaleOnly:
+                    FixedGender = Gender.Female; break;
+                default:
+                    FixedGender = Gender.Genderless; break;
+            }
+        }
+        internal RaidBattleSlot(string name, string form, bool allowHA, int fix)
+        {
+            pokemon = Pokemon.GetPokemon(name, form);
+            allowHiddenAbility = allowHA;
+            FlawlessIVs = fix;
+
+            switch (pokemon.GenderRatio)
+            {
+                case GenderRatio.MaleOnly:
+                    FixedGender = Gender.Male; break;
+                case GenderRatio.FemaleOnly:
+                    FixedGender = Gender.Female; break;
+                default:
+                    FixedGender = Gender.Genderless; break;
+            }
+        }
+    }
+    public class CommonSlot1 : RaidBattleSlot
+    {
+        internal CommonSlot1(string name, Rom limited = Rom.empty) : base(name, false, 1)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+        internal CommonSlot1(string name, string form, Rom limited = Rom.empty) : base(name, form, false, 1)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+    }
+    public class CommonSlot2 : RaidBattleSlot
+    {
+        internal CommonSlot2(string name, Rom limited = Rom.empty) : base(name, false, 2)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+        internal CommonSlot2(string name, string form, Rom limited = Rom.empty) : base(name, form, false, 2)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+    }
+    public class CommonSlot3 : RaidBattleSlot
+    {
+        internal CommonSlot3(string name, Rom limited = Rom.empty) : base(name, false, 3)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+        internal CommonSlot3(string name, string form, Rom limited = Rom.empty) : base(name, form, false, 3)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+    }
+    public class CommonSlot4 : RaidBattleSlot
+    {
+        internal CommonSlot4(string name, Rom limited = Rom.empty) : base(name, false, 4)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+        internal CommonSlot4(string name, string form, Rom limited = Rom.empty) : base(name, form, false, 4)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+    }
+    public class CommonSlot5 : RaidBattleSlot
+    {
+        internal CommonSlot5(string name, Rom limited = Rom.empty) : base(name, true, 4)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+        internal CommonSlot5(string name, string form, Rom limited = Rom.empty) : base(name, form, true, 4)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+    }
+
+    public class EventSlot1 : RaidBattleSlot
+    {
+        internal EventSlot1(string name, Rom limited = Rom.empty) : base(name, true, 1)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+        internal EventSlot1(string name, string form, Rom limited = Rom.empty) : base(name, form, true, 1)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+    }
+    public class EventSlot2 : RaidBattleSlot
+    {
+        internal EventSlot2(string name, Rom limited = Rom.empty) : base(name, true, 2)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+        internal EventSlot2(string name, string form, Rom limited = Rom.empty) : base(name, form, true, 2)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+    }
+    public class EventSlot3 : RaidBattleSlot
+    {
+        internal EventSlot3(string name, Rom limited = Rom.empty) : base(name, true, 3)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+        internal EventSlot3(string name, string form, Rom limited = Rom.empty) : base(name, form, true, 3)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+    }
+    public class EventSlot4 : RaidBattleSlot
+    {
+        internal EventSlot4(string name, Rom limited = Rom.empty) : base(name, true, 4)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+        internal EventSlot4(string name, string form, Rom limited = Rom.empty) : base(name, form, true, 4)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+    }
+    public class EventSlot5 : RaidBattleSlot
+    {
+        internal EventSlot5(string name, Rom limited = Rom.empty) : base(name, true, 5)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+        internal EventSlot5(string name, string form, Rom limited = Rom.empty) : base(name, form, true, 5)
+        {
+            Label = limited != Rom.empty ? $"({limited.ToKanji()})" : "";
+        }
+    }
+}
